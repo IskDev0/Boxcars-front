6a99a336c5e71a04755dc208ba5c5164207081de
Iskander <isk.dev001@gmail.com>
2024-06-02 12:27:19 +0500

    Add: assets and color mode package

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,21 +1,28 @@
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  modules: [
-    '@nuxtjs/tailwindcss',
-    'shadcn-nuxt',
-    '@pinia/nuxt',
-    '@formkit/auto-animate',
-    '@nuxt/image',
-    'nuxt-icon',
-    '@nuxtjs/i18n',
-    'nuxt-swiper',
-    '@vueuse/nuxt',
-  ],
-  shadcn: {
-    prefix: '',
-    componentDir: './components/ui'
-  },
-  pinia: {
-    storesDirs: ['./stores/**'],
-  },
+    devtools: {enabled: true},
+    modules: [
+        '@nuxtjs/tailwindcss',
+        'shadcn-nuxt',
+        '@pinia/nuxt',
+        '@formkit/auto-animate',
+        '@nuxt/image',
+        'nuxt-icon',
+        '@nuxtjs/i18n',
+        'nuxt-swiper',
+        '@vueuse/nuxt',
+        '@nuxtjs/color-mode'
+    ],
+    shadcn: {
+        prefix: '',
+        componentDir: './components/ui'
+    },
+    pinia: {
+        storesDirs: ['./stores/**'],
+    },
+    devServer: {
+        port: 5050
+    },
+    colorMode: {
+        classSuffix: ''
+    }
 })
